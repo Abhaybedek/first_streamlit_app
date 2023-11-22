0c5848e28c6d0205569ab475ee0cc201d7d28e98
Abhaybedek <abhay.bedekar@gmail.com>
2023-11-22 13:06:45 +0530

    Update streamlit_app.py
    one final change

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,5 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinech & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('One Final Change')
 
