0715b276e8353f39577bafc1d97ee206f20eb7e9
Abhaybedek <abhay.bedekar@gmail.com>
2023-11-22 13:03:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit
 streamlit.title('My Parents New healthy dinner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinech & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
